a7734b10311da4757cd02b86e8cf87192e21d6b3
IuliaStefan0209 <stefaniulia99@gmail.com>
2021-11-25 21:30:55 +0200

    aplicatie2

--- proiect/aplicatie2/urls.py
+++ proiect/aplicatie2/urls.py
@@ -4,5 +4,7 @@ from aplicatie2 import views
 app_name = 'aplicatie2'
 
 urlpatterns = [
-    path('', views.CreateLocationView.as_view(), name='adaugare')
+    path('', views.CreateLocationView.as_view(), name='adaugare'),
+    path('<int:pk>/update/', views.UpdateLocationView.as_view(), name='modificare'),
+    path('list/', views.ListLocationView.as_view(), name='listare')
 ]
--- proiect/aplicatie2/views.py
+++ proiect/aplicatie2/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import render
 # DeleteView => stergere date in DB
 # IndexView => informare cu privire la formular
 # ListView => informatii de tip lista din DB
-from django.views.generic import CreateView
+from django.urls import reverse
+from django.views.generic import CreateView, UpdateView, ListView
 
 from aplicatie2.models import Location
 
@@ -18,4 +19,17 @@ class CreateLocationView(CreateView):
     template_name = 'aplicatie2/location_form.html'
 
     def get_success_url(self):
-        return reverse('aplicatie2:adaugare')
+        return reverse('aplicatie2:listare')
+
+class UpdateLocationView(UpdateView):
+    model = Location
+    # fields = '__all__'
+    fields = ['city', 'country']
+    template_name = 'aplicatie2/location_form.html'
+
+    def get_success_url(self):
+        return reverse('aplicatie2:listare')
+
+class ListLocationView(ListView):
+    model = Location
+    template_name = 'aplicatie2/location_index.html'
